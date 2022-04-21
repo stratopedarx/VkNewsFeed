6acc9435ce9634bd29238a16a142eb5d86bd13f4
Sergey Lobanov <Sergey.Lobanov@stacksports.com>
2022-04-21 22:48:23 +0300

    Initial commit

---
+++ VkNewsFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VkNewsFeed
+//
+//  Created by Sergey Lobanov on 21.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
